83516da8181552c9c523a2af7b0d8c6455c0b72e
arc-015 <chavanavinash015@gmail.com>
2022-10-15 10:24:05 +0530

    Welcome to SnakeLadder Game

---
+++ com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake And Ladder Simulator Game");
+    }
+}
